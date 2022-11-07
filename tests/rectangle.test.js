b4fb298aa4f2c4a40137a5a718bdebd3746fc8d8
Adrian Gawroński <adini214@gmail.com>
2022-11-07 20:05:15 +0100

    added one more test

--- tests/rectangle.test.js
+++ tests/rectangle.test.js
@@ -3,20 +3,25 @@ import { getRectangleArea, getRectanglePerimeter, getRectangleInfo } from '../js
 test('should calculate the equation', () => {
     const outputPerimeter = getRectanglePerimeter(5,6);
     expect(outputPerimeter).toBe(22);
-})
+});
 
 test('should output the equation', () => {
     const outputArea = getRectangleArea(7,9);
     expect(outputArea).toBe(63);
-})
+});
 
 test('should display rectangle info', () => {
     const outputInfo = getRectangleInfo(4,5);
     expect(outputInfo).toBe(console.log(`The perimeter of a rectangle is ${18} and the area is ${20}`));
-})
+});
 
 test('value is positive?', () => {
     const outputPositive = getRectanglePerimeter(4,2);
     expect(outputPositive).toBeGreaterThan(0);
 
-})
+});
+
+test('is a value',() => {
+    const outputValue = getRectangleArea(4,8);
+    expect(outputValue).toBeTruthy();
+});
