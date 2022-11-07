4577f4c7887b7e6ad74e7573151876f5f2d7f718
Adrian Gawroński <adini214@gmail.com>
2022-11-07 18:02:00 +0100

    created tests for functions in rectangle.js file

---
+++ tests/rectangle.test.js
@@ -0,0 +1,22 @@
+import { getRectangleArea, getRectanglePerimeter, getRectangleInfo } from '../js/rectangle';
+
+test('should calculate the equation', () => {
+    const outputPerimeter = getRectanglePerimeter(5,6);
+    expect(outputPerimeter).toBe(22);
+})
+
+test('should output the equation', () => {
+    const outputArea = getRectangleArea(7,9);
+    expect(outputArea).toBe(63);
+})
+
+test('should display rectangle info', () => {
+    const outputInfo = getRectangleInfo(4,5);
+    expect(outputInfo).toBe(console.log(`The perimeter of a rectangle is ${18} and the area is ${20}`));
+})
+
+test('value is positive?', () => {
+    const outputPositive = getRectanglePerimeter(4,2);
+    expect(outputPositive).toBeGreaterThan(0);
+
+})
